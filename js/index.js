beff8aa2912073aac4cfa175854cc5c8535b2b7b
aulianafahrian <aulianafahrian@gmail.com>
2023-10-26 15:58:25 +0700

    fix sign up

--- js/index.js
+++
@@ -1,5 +0,0 @@
-import postLogin from "https://cdn.jsdelivr.net/gh/erfahtech/js_erfahtech@main/login.js";
-import logout from "https://cdn.jsdelivr.net/gh/erfahtech/js_erfahtech@main/logout.js";
-
-window.postLogin = postLogin;
-window.logout = logout;
