b8b0fc0bfc081968183fecdd6279a7397ed2d3c1
aulianafahrian <aulianafahrian@gmail.com>
2023-10-26 15:45:42 +0700

    fix logout func

---
+++ js/index.js
@@ -0,0 +1,5 @@
+import postLogin from "https://cdn.jsdelivr.net/gh/erfahtech/js_erfahtech@main/login.js";
+import logout from "https://cdn.jsdelivr.net/gh/erfahtech/js_erfahtech@main/logout.js";
+
+window.postLogin = postLogin;
+window.logout = logout;
--- js/post.js
+++
@@ -1,3 +0,0 @@
-import postLogin from "https://cdn.jsdelivr.net/gh/erfahtech/js_erfahtech@main/login.js";
-
-window.postLogin = postLogin;
