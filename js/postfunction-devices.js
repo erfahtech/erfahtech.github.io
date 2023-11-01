fbb122f18705e93a1c747d9fd107472a6f5b1240
aulianafahrian <aulianafahrian@gmail.com>
2023-11-01 15:42:26 +0700

    fix postDevice

--- js/postfunction-devices.js
+++ js/postfunction-devices.js
@@ -1,17 +1,74 @@
+// import { postWithBearer } from "https://jscroot.github.io/api/croot.js";
+// import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
+// import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+// export default function postDevices() {
+//   let nama = getValue("isiName");
+//   let topic = getValue("isiTopic");
+//   const loadingElement = document.getElementById("loading"); // Get the loading element by its ID
+
+//   document.getElementById('diabutton').style.display = "none";
+
+//   // Show the loading animation
+//   loadingElement.style.display = "block";
+  
+
+//   // Validasi isian tidak boleh kosong
+//   if (nama == "" || topic == "") {
+//     Swal.fire({
+//       icon: "error",
+//       title: "Gagal Menambahkan Device",
+//       text: "Please fill in all fields.",
+//     });
+//     // Hide the loading animation in case of validation error
+//     loadingElement.style.display = "none";
+//     return;
+//   }
+
+//   let target_url = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-insertdevices";
+//   let datainjson = {
+//     name: nama,
+//     topic: topic,
+//   };
+
+//   postWithBearer(target_url, getCookie("token"), datainjson, responseData, () => {
+//     loadingElement.style.display = "none";
+//   });
+// }
+
+// function responseData(result) {
+//   if (result) {
+//     // Tampilkan SweetAlert berhasil signUp
+//     Swal.fire({
+//       icon: "success",
+//       title: "Tambah Device Berhasil",
+//       text: result.message,
+//     }).then((result) => {
+//       if (result.isConfirmed || result.isDismissed) {
+//         window.location.href = "device_control.html";
+//       }
+//     });
+//   } else {
+//     Swal.fire({
+//       icon: "error",
+//       title: "Tambah Device Gagal",
+//       text: result.message,
+//     });
+//   }
+// }
+
 import { postWithBearer } from "https://jscroot.github.io/api/croot.js";
-import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 export default function postDevices() {
-  let nama = getValue("isiName");
-  let topic = getValue("isiTopic");
+  const nama = document.getElementById("isiName").value;
+  const topic = document.getElementById("isiTopic").value;
   const loadingElement = document.getElementById("loading"); // Get the loading element by its ID
 
   document.getElementById('diabutton').style.display = "none";
 
   // Show the loading animation
   loadingElement.style.display = "block";
-  
 
   // Validasi isian tidak boleh kosong
   if (nama == "" || topic == "") {
@@ -56,4 +113,3 @@ function responseData(result) {
     });
   }
 }
-
