9f8f01cff49b563d65ed499808c00f7a29e138c7
erditona <erditonaushaadam@gmail.com>
2023-11-04 17:44:40 +0700

    Add device card to user device control page

--- js/getdevice.js
+++ js/getdevice.js
@@ -3,6 +3,7 @@ import { addInner } from "https://jscroot.github.io/element/croot.js";
 export const URLGetDevice = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-getdevices";
 
 export const cardDevice = `
+<div id="device" class="flex-shrink max-w-full px-4 w-full sm:w-1/2 mb-6">
   <div class="bg-white dark:bg-surfacedark-200 rounded-lg shadow-lg h-full p-6">
     <div class="flex flex-wrap flex-row items-center">
       <div class="flex-shrink max-w-full w-1/2">
@@ -34,6 +35,7 @@ export const cardDevice = `
           height="264"></canvas>
       </div>
     </div>
+  </div>
   </div>
 `;
 
