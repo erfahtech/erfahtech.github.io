b69e65b5ac42398ef9f2394fba0dc30998c7db87
erditona <erditonaushaadam@gmail.com>
2023-11-05 12:47:39 +0700

    Update device card with new values.

--- js/getdevice.js
+++ js/getdevice.js
@@ -7,14 +7,14 @@ export const cardDevice = `
   <div class="bg-white dark:bg-surfacedark-200 rounded-lg shadow-lg h-full p-6">
     <div class="flex flex-wrap flex-row items-center">
       <div class="flex-shrink max-w-full w-1/2">
-        <h5 class="text-gray-500 mb-1">${isi.topic}</h5>
+        <h5 class="text-gray-500 mb-1">${value.topic}</h5>
         <h3 class="text-lg font-bold mb-1">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
             class="inline-block h-4 w-4 mr-2 bi bi-thermometer" viewBox="0 0 16 16">
             <path d="M8 14a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
             <path d="M8 0a2.5 2.5 0 0 0-2.5 2.5v7.55a3.5 3.5 0 1 0 5 0V2.5A2.5 2.5 0 0 0 8 0zM6.5 2.5a1.5 1.5 0 1 1 3 0v7.987l.167.15a2.5 2.5 0 1 1-3.333 0l.166-.15V2.5z" />
           </svg>
-          ${isi.name}
+          ${value.name}
         </h3>
         <p class="text-sm text-green-500">
           <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
@@ -45,6 +45,6 @@ export function responseData(results) {
 }
 
 export function isiCard(value) {
-  const content = cardDevice.replace(`${isi.topic}`, value.topic).replace(`${isi.name}`, value.name);
+  const content = cardDevice.replace(`${value.topic}`, value.topic).replace(`${value.name}`, value.name);
   addInner("devices", content);
 }
