6c6c2e4a8845b4b72e10ed6e40323f3682d13200
erditona <erditonaushaadam@gmail.com>
2023-11-05 13:21:39 +0700

    Mengembalikan

--- js/getdevice.js
+++ js/getdevice.js
@@ -2,19 +2,19 @@ import { addInner } from "https://jscroot.github.io/element/croot.js";
 
 export const URLGetDevice = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-getdevices";
 
-export const cardDevice = (results) => `
+export const cardDevice = `
 <div id="device" class="flex-shrink max-w-full px-4 w-full sm:w-1/2 mb-6">
   <div class="bg-white dark:bg-surfacedark-200 rounded-lg shadow-lg h-full p-6">
     <div class="flex flex-wrap flex-row items-center">
       <div class="flex-shrink max-w-full w-1/2">
-        <h5 class="text-gray-500 mb-1">${results.data.topic}</h5>
+        <h5 class="text-gray-500 mb-1">#TOPIC#</h5>
         <h3 class="text-lg font-bold mb-1">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
             class="inline-block h-4 w-4 mr-2 bi bi-thermometer" viewBox="0 0 16 16">
             <path d="M8 14a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
             <path d="M8 0a2.5 2.5 0 0 0-2.5 2.5v7.55a3.5 3.5 0 1 0 5 0V2.5A2.5 2.5 0 0 0 8 0zM6.5 2.5a1.5 1.5 0 1 1 3 0v7.987l.167.15a2.5 2.5 0 1 1-3.333 0l.166-.15V2.5z" />
           </svg>
-          ${results.data.name}
+          #NAME#
         </h3>
         <p class="text-sm text-green-500">
           <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
@@ -36,11 +36,16 @@ export const cardDevice = (results) => `
       </div>
     </div>
   </div>
-</div>
+  </div>
 `;
 
 export function responseData(results) {
   console.log(results);
-  const content = cardDevice(results.data);
+  results.data.forEach(isiCard);
+}
+
+export function isiCard(value) {
+  const content = cardDevice.replace("#TOPIC#", value.topic).replace("#NAME#", value.name);
+  // .replace("#STATUS#", value.status);
   addInner("devices", content);
 }
