3057cbdcb0efa895cb8d2c59bcdbbcfc9f75379d
aulianafahrian <aulianafahrian@gmail.com>
2023-11-06 08:20:47 +0700

    Update device icon in getdevice.js

--- js/getdevice.js
+++ js/getdevice.js
@@ -58,7 +58,7 @@ export function isiCard(value) {
           <h5 class="text-gray-500 mb-1">${topic}</h5>
           <h3 class="text-lg font-bold mb-1">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-              class="inline-block h-4 w-4 mr-2 bi bi-thermometer" viewBox="0 0 16 16">
+              class="inline-block h-4 w-4 mr-2 bi bi-lightbulb" viewBox="0 0 16 16">
               <path d="M8 14a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
               <path d="M8 0a2.5 2.5 0 0 0-2.5 2.5v7.55a3.5 3.5 0 1 0 5 0V2.5A2.5 2.5 0 0 0 8 0zM6.5 2.5a1.5 1.5 0 1 1 3 0v7.987l.167.15a2.5 2.5 0 1 1-3.333 0l.166-.15V2.5z" />
             </svg>
