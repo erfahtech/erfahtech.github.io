958f85e0e3ae4910b8ce15eaeaaca590b752cb4f
erditona <erditonaushaadam@gmail.com>
2023-11-06 18:26:59 +0700

    Add toggle switch for MQTT device control

--- js/getdevice.js
+++ js/getdevice.js
@@ -28,11 +28,29 @@ export function responseData(results) {
   results.data.forEach(isiCard);
 }
 
-function isiCard(value) {
+export function isiCard(value) {
   const topic = value.topic;
   const name = value.name;
 
-  // Create cardDiv element
+  // Buat elemen toggle switch
+  const toggleSwitch = document.createElement("div");
+  toggleSwitch.className = "toggle-switch relative inline-flex w-[52px] h-1 mb-6";
+
+  const input = document.createElement("input");
+  input.className = "toggle-checkbox";
+  input.type = "checkbox";
+  input.checked = true;
+  input.style.display = "none"; // Sembunyikan checkbox
+
+  toggleSwitch.appendChild(input);
+
+  const label = document.createElement("label");
+  label.className = "toggle-icon relative block w-12 h-8 rounded-full transition-color duration-150 ease-out";
+  label.setAttribute("for", `toggle-${topic}`);
+
+  toggleSwitch.appendChild(label);
+
+  // Tambahkan elemen toggle switch ke container
   const cardDiv = document.createElement("div");
   cardDiv.className = "flex-shrink max-w-full px-4 w-full sm:w-1/2 mb-6";
   cardDiv.innerHTML = `
@@ -42,19 +60,21 @@ function isiCard(value) {
           <h5 class="text-gray-500 mb-1">${topic}</h5>
           <h3 class="text-lg font-bold mb-1">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-              class="inline-block h-4 w-4 mr-2 bi bi-thermometer" viewBox="0 0 16 16">
+              class="inline-block h-4 w-4 mr-2 bi bi-lightbulb" viewBox="0 0 16 16">
               <path d="M8 14a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
               <path d="M8 0a2.5 2.5 0 0 0-2.5 2.5v7.55a3.5 3.5 0 1 0 5 0V2.5A2.5 2.5 0 0 0 8 0zM6.5 2.5a1.5 1.5 0 1 1 3 0v7.987l.167.15a2.5 2.5 0 1 1-3.333 0l.166-.15V2.5z" />
             </svg>
             ${name}
           </h3>
           <p class="text-sm text-green-500">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-              class="inline-block w-4 h-4 bi bi-arrow-up-short" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"></path>
-            </svg>
-            On
-          </p>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                class="inline-block w-4 h-4 bi bi-arrow-down-short" viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                  d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z">
+                </path>
+              </svg>
+               On
+            </p>
           <br />
         </div>
         <div class="flex-shrink max-w-full w-1/2">
@@ -62,32 +82,25 @@ function isiCard(value) {
             height="264"></canvas>
         </div>
       </div>
-      <div class="toggle-switch">
-        <input type="checkbox" class="toggle-checkbox" id="toggle-${topic}">
-        <label for="toggle-${topic}"></label>
-      </div>
     </div>
   `;
 
-  const input = cardDiv.querySelector(".toggle-checkbox"); // Get the input element
+  cardDiv.querySelector(".flex-shrink").appendChild(toggleSwitch);
+  devicesContainer.appendChild(cardDiv);
 
-  cardDiv.addEventListener("click", () => {
-    if (input) {
-      // Toggle the checkbox and perform any other desired action
-      input.checked = !input.checked;
+  toggleSwitch.addEventListener("click", (event) => {
+    const input = event.currentTarget.querySelector("input");
+    input.checked = !input.checked;
 
-      const payload = input.checked ? "1" : "0";
+    const payload = input.checked ? "1" : "0";
 
-      if (mqttClient && mqttClient.connected) {
-        mqttClient.publish(topic, payload);
-        console.log(`Mengirim payload ${payload} ke topik ${topic}`);
-      } else {
-        console.error("Koneksi MQTT tidak aktif");
-      }
+    if (mqttClient && mqttClient.connected) {
+      mqttClient.publish(topic, payload);
+      console.log(`Mengirim payload ${payload} ke topik ${topic}`);
+    } else {
+      console.error("Koneksi MQTT tidak aktif");
     }
   });
-
-  devicesContainer.appendChild(cardDiv);
 }
 
 // document.addEventListener("DOMContentLoaded", function () {
