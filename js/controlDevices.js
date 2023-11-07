88bca5b6c8aa6752d0387d543266fa84e0694c8d
erditona <erditonaushaadam@gmail.com>
2023-11-07 15:34:20 +0700

    Update delete and edit button styles in controlDevices.js

--- js/controlDevices.js
+++ js/controlDevices.js
@@ -28,12 +28,12 @@ export function isiCard(value) {
 
   // Buat tombol delete dan edit
   const deleteButton = document.createElement("button");
-  deleteButton.className = "delete-button";
+  deleteButton.className = "delete-button p-1 bg-red-500 text-white rounded";
   deleteButton.id = `delete-${topic}`;
   deleteButton.textContent = "Delete";
 
   const editButton = document.createElement("button");
-  editButton.className = "edit-button";
+  editButton.className = "edit-button mr-2 p-1 bg-blue-500 text-white rounded";
   editButton.id = `edit-${topic}`;
   editButton.textContent = "Edit";
 
@@ -61,35 +61,35 @@ export function isiCard(value) {
   cardDiv.className = "flex-shrink max-w-full px-4 w-full sm:w-1/2 mb-6";
   cardDiv.innerHTML = `
   <div class="bg-white dark-bg-surfacedark-200 rounded-lg shadow-lg h-full p-6">
-  <div class="flex flex-wrap flex-row items-center">
-      <div class="flex-shrink max-w-full w-1/2">
-          <h5 class="text-gray-500 mb-1">${topic}</h5>
-          <h3 class="text-lg font-bold mb-1">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                  class="inline-block ltr:mr-2 rtl:ml-2 -mt-1 bi bi-cpu" viewBox="0 0 16 16">
-                  <path
-                      d="M5 0a.5.5 0 0 1 .5.5V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2A2.5 2.5 0 0 1 14 4.5h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14a2.5 2.5 0 0 1-2.5 2.5v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14A2.5 2.5 0 0 1 2 11.5H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2A2.5 2.5 0 0 1 4.5 2V.5A.5.5 0 0 1 5 0zm-.5 3A1.5 1.5 0 0 0 3 4.5v7A1.5 1.5 0 0 0 4.5 13h7a1.5 1.5 0 0 0 1.5-1.5v-7A1.5 1.5 0 0 0 11.5 3h-7zM5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3zM6.5 6a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z" />
-              </svg>
-              ${name}
-          </h3>
-          <p class="status text-sm text-green-500">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                  class="inline-block ltr:mr-2 rtl:ml-2 -mt-1 bi bi-broadcast"
-                  viewBox="0 0 16 16">
-                  <path
-                      d="M3.05 3.05a7 7 0 0 0 0 9.9.5.5 0 0 1-.707.707 8 8 0 0 1 0-11.314.5.5 0 0 1 .707.707zm2.122 2.122a4 4 0 0 0 0 5.656.5.5 0 1 1-.708.708 5 5 0 0 1 0-7.072.5.5 0 0 1 .708.708zm5.656-.708a.5.5 0 0 1 .708 0 5 5 0 0 1 0 7.072.5.5 0 1 1-.708-.708 4 4 0 0 0 0-5.656.5.5 0 0 1 0-.708zm2.122-2.12a.5.5 0 0 1 .707 0 8 8 0 0 1 0 11.313.5.5 0 0 1-.707-.707 7 7 0 0 0 0-9.9.5.5 0 0 1 0-.707zM10 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z" />
-              </svg>
-              <span id="status-${topic}">OFF</span>
-          </p>
-          <br />
-      </div>
-      <div class="flex-shrink max-w-full w-1/2">
-          <canvas class="max-w-100" id="LineAreaSm"
-              style="display: block; box-sizing: border-box; height: 66px; width: 132px"
-              width="528" height="264"></canvas>
-      </div>
+    <div class="flex flex-wrap flex-row items-center">
+        <div class="flex-shrink max-w-full w-1/2">
+            <h5 class="text-gray-500 mb-1">${topic}</h5>
+            <h3 class="text-lg font-bold mb-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="inline-block ltr:mr-2 rtl:ml-2 -mt-1 bi bi-cpu" viewBox="0 0 16 16">
+                    <path
+                        d="M5 0a.5.5 0 0 1 .5.5V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2A2.5 2.5 0 0 1 14 4.5h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14a2.5 2.5 0 0 1-2.5 2.5v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14A2.5 2.5 0 0 1 2 11.5H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2A2.5 2.5 0 0 1 4.5 2V.5A.5.5 0 0 1 5 0zm-.5 3A1.5 1.5 0 0 0 3 4.5v7A1.5 1.5 0 0 0 4.5 13h7a1.5 1.5 0 0 0 1.5-1.5v-7A1.5 1.5 0 0 0 11.5 3h-7zM5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3zM6.5 6a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z" />
+                </svg>
+                ${name}
+            </h3>
+            <p class="status text-sm text-green-500">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="inline-block ltr:mr-2 rtl:ml-2 -mt-1 bi bi-broadcast"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M3.05 3.05a7 7 0 0 0 0 9.9.5.5 0 0 1-.707.707 8 8 0 0 1 0-11.314.5.5 0 0 1 .707.707zm2.122 2.122a4 4 0 0 0 0 5.656.5.5 0 1 1-.708.708 5 5 0 0 1 0-7.072.5.5 0 0 1 .708.708zm5.656-.708a.5.5 0 0 1 .708 0 5 5 0 0 1 0 7.072.5.5 0 1 1-.708-.708 4 4 0 0 0 0-5.656.5.5 0 0 1 0-.708zm2.122-2.12a.5.5 0 0 1 .707 0 8 8 0 0 1 0 11.313.5.5 0 0 1-.707-.707 7 7 0 0 0 0-9.9.5.5 0 0 1 0-.707zM10 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z" />
+                </svg>
+                <span id="status-${topic}">OFF</span>
+            </p>
+            <br />
+        </div>
+        <div class="flex-shrink max-w-full w-1/2">
+            <canvas class="max-w-100" id="LineAreaSm"
+                style="display: block; box-sizing: border-box; height: 66px; width: 132px"
+                width="528" height="264"></canvas>
+        </div>
+    </div>
   </div>
-</div>
   `;
 
   cardDiv.querySelector(".flex-shrink").appendChild(toggleSwitch);
