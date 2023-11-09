fee07f3ef23030931b19ca1868801b87617d3420
erditona <erditonaushaadam@gmail.com>
2023-11-09 14:33:50 +0700

    add mqttConnection.js

--- js/mqttConnection.js
+++ js/mqttConnection.js
@@ -1,12 +1,18 @@
+// Mendefinisikan URL broker MQTT yang akan dihubungi
 const brokerUrl = "wss://broker.emqx.io:8084/mqtt";
+
+// Membuat koneksi MQTT menggunakan URL broker
 const mqttClient = mqtt.connect(brokerUrl);
 
+// Menetapkan event handler ketika koneksi berhasil terhubung
 mqttClient.on("connect", () => {
   console.log("Terhubung ke broker MQTT");
 });
 
+// Menetapkan event handler untuk menangani kesalahan koneksi MQTT
 mqttClient.on("error", (error) => {
   console.error("Kesalahan koneksi MQTT:", error);
 });
 
+// Mengekspor objek klien MQTT untuk digunakan di file lain
 export default mqttClient;
