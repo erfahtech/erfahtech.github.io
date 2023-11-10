7d4d5c064e346000096fb217911ffa2baeebdc37
erditona <erditonaushaadam@gmail.com>
2023-11-10 20:22:32 +0700

    Remove commented out code in mqttSubscribe.js

--- js/mqttSubscribe.js
+++ js/mqttSubscribe.js
@@ -38,36 +38,3 @@ mqttClient.on("message", (topic, message) => {
 mqttClient.on("error", (error) => {
   console.error("Kesalahan koneksi MQTT:", error);
 });
-
-// // main.js
-// import mqttClient from "./mqttConnection.js";
-
-// // Function to create a card with the received message
-// // function createMessageCard(message) {
-// //   const cardDiv = document.createElement("div");
-// //   cardDiv.className = "message-card";
-// //   cardDiv.innerHTML = `<p>${message}</p>`;
-// //   document.getElementById("messagesContainer").appendChild(cardDiv);
-// // }
-
-// // Subscribe to a topic
-// const topicToSubscribe = "urse/#"; // Change this to your topic
-// mqttClient.subscribe(topicToSubscribe);
-// console.log(`Berlangganan ke topik ${topicToSubscribe}`);
-
-// // Listen for incoming messages on the subscribed topic
-// mqttClient.on("message", (topic, message) => {
-//   const receivedMessage = message.toString();
-//   console.log(`Received message on topic ${topic}: ${receivedMessage}`);
-
-//   // Display the received message in a card
-//   //   createMessageCard(receivedMessage);
-// });
-
-// // Handle errors in MQTT connection
-// mqttClient.on("error", (error) => {
-//   console.error("Kesalahan koneksi MQTT:", error);
-// });
-
-// window.mqttClient = mqttClient;
-// window.topicToSubscribe = topicToSubscribe;
