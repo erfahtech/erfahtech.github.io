e38b85bacd0676a03e1c406cfec9569c10ab50b7
aulianafahrian <aulianafahrian@gmail.com>
2023-11-10 22:46:48 +0700

    Update tableDevice in gettbl.js

--- js/gettbl.js
+++ js/gettbl.js
@@ -2,77 +2,56 @@ import { addInner } from "https://jscroot.github.io/element/croot.js";
 
 export const URLGetDevice = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-getdevices";
 
+export const tableDevice = `
+<tr class="h-18 border-b border-coolGray-100">
+    <td class="whitespace-nowrap px-6 bg-white text-left">
+        <div class="flex items-center">
+                <p class=" font-semibold text-coolGray-800">
+                #NAME#
+                </p>
+    </td>
+    <td class="whitespace-nowrap px-6 bg-white text-left">
+    <div class="flex items-center">
+            <p class=" font-semibold text-coolGray-500">#TOPIC#</p>
+    </div>
+    </td>
+    <td class="whitespace-nowrap px-6 bg-white text-sm font-medium text-coolGray-800 text-center">
+     <button class="edit-button">Edit</button>
+     <button class="delete-button">Delete</button>
+     </td>
+    </td>
+</tr>
+`;
+
 // export const tableDevice = `
 // <tr class="h-18 border-b border-coolGray-100">
 //     <td class="whitespace-nowrap px-6 bg-white text-left">
 //         <div class="flex items-center">
-//                 <p class=" font-semibold text-coolGray-800">
+//             <p class=" font-semibold text-coolGray-800">
 //                 #NAME#
-//                 </p>
+//             </p>
+//         </div>
 //     </td>
 //     <td class="whitespace-nowrap px-6 bg-white text-left">
-//     <div class="flex items-center">
+//         <div class="flex items-center">
 //             <p class=" font-semibold text-coolGray-500">#TOPIC#</p>
-//     </div>
+//         </div>
 //     </td>
 //     <td class="whitespace-nowrap px-6 bg-white text-sm font-medium text-coolGray-800 text-center">
-//     <a type="button" href="#" class="mr-4" title="Edit">
+//         <button id="edit_button" title="Edit">
 //             <span class="material-symbols-outlined">
-//             edit
+//                 edit
 //             </span>
-//         </a>
+//         </button>
 //         <button type="button" id="del_button" onclick="" title="Delete">
 //             <span class="material-symbols-outlined">
-//             delete
+//                 delete
 //             </span>
 //         </button>
 //     </td>
 // </tr>
 // `;
 
-export const tableDevice = `
-<tr class="h-18 border-b border-coolGray-100">
-    <td class="whitespace-nowrap px-6 bg-white text-left">
-        <div class="flex items-center">
-            <p class=" font-semibold text-coolGray-800">
-                #NAME#
-            </p>
-        </div>
-    </td>
-    <td class="whitespace-nowrap px-6 bg-white text-left">
-        <div class="flex items-center">
-            <p class=" font-semibold text-coolGray-500">#TOPIC#</p>
-        </div>
-    </td>
-    <td class="whitespace-nowrap px-6 bg-white text-sm font-medium text-coolGray-800 text-center">
-        <button class="mr-4 edit-button" title="Edit">
-            <span class="material-symbols-outlined">
-                edit
-            </span>
-        </button>
-        <button type="button" id="del_button" onclick="" title="Delete">
-            <span class="material-symbols-outlined">
-                delete
-            </span>
-        </button>
-    </td>
-</tr>
-`;
-
-export function showEditDialog(index) {
-    const dialog = document.getElementById('edit-dialog');
-    dialog.style.display = 'block';
-    const device = tableDevice.devices[index];
-    const nameInput = document.getElementById('device-name');
-    const topicInput = document.getElementById('device-topic');
-    nameInput.value = device.name;
-    topicInput.value = device.topic;
-}
-
-export function closeEditDialog() {
-    const dialog = document.getElementById('edit-dialog');
-    dialog.style.display = 'none';
-}
 
 
 export function responseData(results) {
