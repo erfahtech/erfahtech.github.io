8cf53c8adf0cfc72987246015139020a36285936
aulianafahrian <aulianafahrian@gmail.com>
2023-11-11 16:09:53 +0700

    Replace id with user in tableDevice content

--- js/gettbl2.js
+++ js/gettbl2.js
@@ -50,7 +50,7 @@ export function isiTable(value) {
     const content = 
     tableDevice.replace("#TOPIC#", value.topic)
                 .replace("#NAME#", value.name)
-                .replace("#IDHAPUS#", value.id);
+                .replace("#IDHAPUS#", value.user);
     addInner("device", content);
 
     const checkbox = document.querySelector('input[name="check_a"]');
