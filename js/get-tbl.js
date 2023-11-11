8be6546fe18f2174d5ce4f701ef6cadf7b72288c
aulianafahrian <aulianafahrian@gmail.com>
2023-11-11 12:59:53 +0700

    Refactor get-tbl.js and get-tbl2.js

--- js/get-tbl.js
+++ js/get-tbl.js
@@ -2,24 +2,21 @@
 import { URLGetDevice, responseData } from "./gettbl.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
-document.addEventListener('DOMContentLoaded', function () {
-  const get = (target_url, responseFunction) => {
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer " + getCookie("token"));
 
-    const requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
-    };
+const get = (target_url, responseFunction) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer " + getCookie("token"));
 
-    fetch(target_url, requestOptions)
-      .then((response) => response.text())
-      .then((result) => responseFunction(JSON.parse(result)))
-      .catch((error) => console.log("error", error));
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
   };
-});
 
-
+  fetch(target_url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => responseFunction(JSON.parse(result)))
+    .catch((error) => console.log("error", error));
+};
 
 get(URLGetDevice, responseData);
