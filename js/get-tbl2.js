f912ee4569b3487b2f0aa41adc2cbade6148bd36
aulianafahrian <aulianafahrian@gmail.com>
2023-11-11 12:37:53 +0700

    Add get-tbl2.js file and import necessary functions

---
+++ js/get-tbl2.js
@@ -0,0 +1,21 @@
+
+import { URLGetDevice, responseData } from "./gettbl2.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+const get = (target_url, responseFunction) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer " + getCookie("token"));
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(target_url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => responseFunction(JSON.parse(result)))
+    .catch((error) => console.log("error", error));
+};
+
+get(URLGetDevice, responseData);
---
+++ js/gettbl2.js
@@ -0,0 +1,41 @@
+import { addInner } from "https://jscroot.github.io/element/croot.js";
+
+export const URLGetDevice = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-getdevices";
+
+export const tableDevice = `
+<tr data-index="0">
+<td>
+    <div class="flex items-center justify-center">
+        <input type="checkbox" name="check_a"
+            class="checked-item w-[18px] h-[18px] accent-primary-600 hover:accent-primary-600 dark:accent-primary-200 rounded-[2px] cursor-pointer">
+    </div>
+</td>
+<td>
+    <div class="flex items-center gap-3">
+        <div class="flex flex-col">
+            <h4 class="font-semibold">#NAMA#.</h4>
+        </div>
+    </div>
+</td>
+<td>
+    <span
+        class="inline-flex items-center h-6 px-3 text-label-md text-pink-700 dark:text-pink-200 bg-pink-100 dark:bg-opacity-20 rounded-full">
+        #TOPIC#
+    </span>
+</td>
+<td><a href="#"
+        class="hover:text-primary-600 dark:hover:text-primary-200">edit</a>
+</td>
+</tr>
+`;
+
+export function responseData(results) {
+    console.log(results);
+    results.data.forEach(isiTable);
+}
+
+export function isiTable(value) {
+    const content = tableDevice.replace("#TOPIC#", value.topic).replace("#NAME#", value.name);
+    addInner("device", content);
+
+}
