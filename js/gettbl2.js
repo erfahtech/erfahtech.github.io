e50a3004ab56995a7f34fbde929c2c285d4ae1ee
erditona <erditonaushaadam@gmail.com>
2023-11-12 11:24:46 +0700

    Fix typo in gettbl2.js

--- js/gettbl2.js
+++ js/gettbl2.js
@@ -45,6 +45,6 @@ export function responseData(results) {
 }
 
 export function isiTable(value) {
-  const content = tableDevice.replace("#TOPIC#", value.topic).replace("#NAME#", value.name).replace("#IDHAPUS#", value._id);
+  const content = tableDevice.replace("#TOPIC#", value.topic).replace("#NAME#", value.name).replace("#IDHAPUS#", value.id);
   addInner("devices", content);
 }
