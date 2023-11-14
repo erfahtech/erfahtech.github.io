27f7e5c81b21596745604d74f14a4cce893b958f
aulianafahrian <aulianafahrian@gmail.com>
2023-11-14 17:13:08 +0700

    Refactored searchDevice function to improve search functionality

--- js/gettabelsetting.js
+++ js/gettabelsetting.js
@@ -4,31 +4,31 @@ export const URLGetDevice = "https://asia-southeast2-urse-project.cloudfunctions
 
 export const tableDevice = `
 <tr>
-<td>
-    <div class="flex items-center gap-3">
-        <div class="flex flex-col">
-            <h4 class="font-semibold">#NAME#.</h4>
+    <td>
+        <div class="flex items-center gap-3">
+            <div class="flex flex-col">
+                <h4 class="font-semibold">#NAME#.</h4>
+            </div>
         </div>
-    </div>
-</td>
-<td>
-    <span
-        class="inline-flex items-center h-6 px-3 text-label-md text-pink-700 dark:text-pink-200 bg-pink-100 dark:bg-opacity-20 rounded-full">
-        #TOPIC#
-    </span>
-</td>
-<td class="whitespace-nowrap px-6 bg-white dark:bg-surfacedark-200 text-sm font-medium text-coolGray-800 text-center">
-    <button onclick="editDevice('#IDEDIT#', '#NAME#', '#TOPIC#')" title="Edit">
-        <span class="material-symbols-outlined">
-            edit
+    </td>
+    <td>
+        <span
+            class="inline-flex items-center h-6 px-3 text-label-md text-pink-700 dark:text-pink-200 bg-pink-100 dark:bg-opacity-20 rounded-full">
+            #TOPIC#
         </span>
-    </button>
-    <button onclick="deleteDevice('#IDHAPUS#')" title="Delete">
-        <span class="material-symbols-outlined">
-        delete
-        </span>
-    </button>
-</td>
+    </td>
+    <td class="whitespace-nowrap px-6 bg-white dark:bg-surfacedark-200 text-sm font-medium text-coolGray-800 text-center">
+        <button onclick="editDevice('#IDEDIT#', '#NAME#', '#TOPIC#')" title="Edit">
+            <span class="material-symbols-outlined">
+                edit
+            </span>
+        </button>
+        <button onclick="deleteDevice('#IDHAPUS#')" title="Delete">
+            <span class="material-symbols-outlined">
+            delete
+            </span>
+        </button>
+    </td>
 </tr>
 `;
 
--- js/search/searchDevice.js
+++ js/search/searchDevice.js
@@ -1,13 +1,35 @@
+// function searchDevice() {
+//     var input, table, tr, td, i, txtValue;
+//     input = document.getElementById("search");
+//     table = document.getElementsByTagName("tbody");
+//     tr = table.getElementsByTagName("tr");
+//     for (i = 0; i < tr.length; i++) {
+//         td = tr[i].getElementsByTagName("td")[0];
+//         if (td) {
+//             txtValue = td.textContent || td.innerText;
+//             if (txtValue.toUpperCase().indexOf(input.value.toUpperCase()) > -1) {
+//                 tr[i].style.display = "";
+//             } else {
+//                 tr[i].style.display = "none";
+//             }
+//         }
+//     }
+// }
+
 function searchDevice() {
-    var input, table, tr, td, i, txtValue;
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("search");
-    table = document.getElementsByTagName("tbody");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("devices");
     tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[0];
         if (td) {
             txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(input.value.toUpperCase()) > -1) {
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
