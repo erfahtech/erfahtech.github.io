8e813332d46e2a39f00c39324fa8cd194881b078
aulianafahrian <aulianafahrian@gmail.com>
2023-11-14 10:29:13 +0700

    Refactor confirmation dialog and update profile heading

--- js/edit/editDevice2.js
+++ js/edit/editDevice2.js
@@ -58,7 +58,10 @@ const editDevice = async (IDEDIT, NAME, TOPIC) => {
         const [newName, newTopic] = combinedInput;
         const isConfirmed = await Swal.fire({
             title: `Konfirmasi`,
-            text: `Apakah Anda yakin mengubah\nNama: ${newName}\nTopic: ${newTopic}`,
+            //text: `Apakah Anda yakin mengubah?\nNama: ${newName}\nTopic: ${newTopic}`,
+            html: `<div>Apakah Anda yakin mengubah?</div>
+                    <div>Nama: ${newName}</div>
+                    <div>Topic: ${newTopic}</div>`,
             icon: "warning",
             showCancelButton: true,
             confirmButtonColor: "#3085d6",
