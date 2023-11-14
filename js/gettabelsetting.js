e9471b63e2425a3471a43a0332893d0ddf1c1cf4
aulianafahrian <aulianafahrian@gmail.com>
2023-11-14 17:54:12 +0700

    Fix typo in tableDevice component

--- js/gettabelsetting.js
+++ js/gettabelsetting.js
@@ -7,7 +7,7 @@ export const tableDevice = `
     <td>
         <div class="flex items-center gap-3">
             <div class="flex flex-col">
-                <h4 class="font-semibold">#NAME#.</h4>
+                <h4 class="font-semibold">#NAME#</h4>
             </div>
         </div>
     </td>
