d18f9d04d1206a1b3e006b126281feef024c95d1
aulianafahrian <aulianafahrian@gmail.com>
2023-11-14 17:53:08 +0700

    Add search functionality to device table

--- js/search/searchDevices.js
+++ js/search/searchDevices.js
@@ -1,21 +1,3 @@
-// function searchDevice() {
-//     var input, table, tr, td, i, txtValue;
-//     input = document.getElementById("search");
-//     table = document.getElementsByTagName("tbody");
-//     tr = table.getElementsByTagName("tr");
-//     for (i = 0; i < tr.length; i++) {
-//         td = tr[i].getElementsByTagName("td")[0];
-//         if (td) {
-//             txtValue = td.textContent || td.innerText;
-//             if (txtValue.toUpperCase().indexOf(input.value.toUpperCase()) > -1) {
-//                 tr[i].style.display = "";
-//             } else {
-//                 tr[i].style.display = "none";
-//             }
-//         }
-//     }
-// }
-
 function searchDevice() {
     // Declare variables
     var input, filter, table, tr, td, i, txtValue;
