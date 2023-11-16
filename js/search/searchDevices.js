166dd2bea03f594c3ba381ac068e543151ff8acb
aulianafahrian <aulianafahrian@gmail.com>
2023-11-16 21:04:34 +0700

    Refactor searchDevice function to use const and let instead of var and update loop to use arrow function

--- js/search/searchDevices.js
+++ js/search/searchDevices.js
@@ -1,22 +1,18 @@
 const searchDevice = () => {
     // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("search");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("devices");
-    tr = table.getElementsByTagName("tr");
+    const input = document.getElementById("search");
+    const filter = input.value.toUpperCase();
+    const table = document.getElementById("devices");
+    const tr = table.getElementsByTagName("tr");
 
     // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
+    for (let i = 0; i < tr.length; i++) {
+        const td = tr[i].getElementsByTagName("td")[0];
         if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
+            const txtValue = td.textContent || td.innerText;
+            tr[i].style.display = txtValue.toUpperCase().indexOf(filter) > -1 ? "" : "none";
         }
     }
-}
+};
+
 window.searchDevice = searchDevice;
