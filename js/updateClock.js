01aae6f4ef2a108d6a1d42cacb8001e33707c31d
aulianafahrian <aulianafahrian@gmail.com>
2023-11-16 20:36:01 +0700

    Update clock and user greeting, remove weather functionality

--- js/updateClock.js
+++ js/updateClock.js
@@ -1,33 +1,13 @@
-// const  updateClock = () => {
-//   var currentTime = new Date();
-//   var hours = currentTime.getHours();
-//   var minutes = currentTime.getMinutes();
-//   var seconds = currentTime.getSeconds();
+// Mengambil nilai dari local storage dengan kunci userEmail
+const userEmail = localStorage.getItem('userEmail');
 
-//   var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//   var day = dayNames[currentTime.getDay()];
+// Mengganti teks dalam elemen dengan ID userGreeting
+const userGreetingElement = document.getElementById('userGreeting');
 
-//   var date = currentTime.getDate();
-//   var month = currentTime.getMonth() + 1; // January is 0!
-//   var year = currentTime.getFullYear();
-
-//   // Pad single digit minutes and seconds with a leading zero
-//   minutes = (minutes < 10 ? "0" : "") + minutes;
-//   seconds = (seconds < 10 ? "0" : "") + seconds;
-
-//   // Set the content of the elements with the current time and date
-//   document.getElementById("current-time").innerText = hours + ":" + minutes + " " + (hours >= 12 ? "PM" : "AM");
-//   document.getElementById("current-day").innerText = day;
-//   document.getElementById("current-date").innerText = month + "/" + date + "/" + year;
-
-//   // Update the clock every second
-//   setTimeout(updateClock, 1000);
-// }
-
-// // Initial call to update the clock
-// updateClock();
-
-
+// Memastikan userEmail tidak kosong sebelum mengganti teks
+if (userEmail) {
+  userGreetingElement.textContent = `Selamat Datang, ${userEmail}!`;
+}
 
 const updateClock = () => {
   const updateTime = () => {
