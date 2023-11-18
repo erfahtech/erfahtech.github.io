a41ff7bb12cf67663318c9d638b5d9e9b4a13259
erditona <erditonaushaadam@gmail.com>
2023-11-18 13:09:47 +0700

    Add logPublish.js file for inserting history data

---
+++ js/logPublish.js
@@ -0,0 +1,25 @@
+import { postWithBearer } from "https://jscroot.github.io/api/croot.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+const insertHistory = (topic, suhu, humidity) => {
+  const target_url = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-inserthistory";
+  const datainjson = {
+    name: "monitoring",
+    topic: topic,
+    payload: "humidity: " + humidity + "," + "suhu: " + suhu,
+  };
+
+  postWithBearer(target_url, getCookie("token"), datainjson, (result) => {
+    responseData(result);
+  });
+};
+
+const responseData = (result) => {
+  if (result) {
+    console.log("Tambah Device Berhasil:", result.message);
+  } else {
+    console.error("Tambah Device Gagal:", result.message);
+  }
+};
+
+export { insertHistory };
