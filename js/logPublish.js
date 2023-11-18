a16e3e503e1f57dddc3a0ace97f3a946d44a06ad
erditona <erditonaushaadam@gmail.com>
2023-11-18 13:45:14 +0700

    Update payload value based on condition

--- js/logPublish.js
+++ js/logPublish.js
@@ -3,10 +3,12 @@ import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 const logPublish = (name, topic, payload) => {
   const target_url = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-inserthistory";
+  // Mengubah nilai payload sesuai dengan kondisi
+  const payloadValue = payload === 1 ? "Menyala" : "Mati";
   const datainjson = {
     name: name,
     topic: topic,
-    payload: payload,
+    payload: payloadValue,
   };
 
   postWithBearer(target_url, getCookie("token"), datainjson, (result) => {
