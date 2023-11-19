a12ebd5d14b055da6b5b4663c54b27b5061c9810
aulianafahrian <aulianafahrian@gmail.com>
2023-11-19 07:23:37 +0700

    Update tableDevice styling in gettabelsetting.js

--- js/gettabelsetting.js
+++ js/gettabelsetting.js
@@ -5,10 +5,9 @@ export const URLGetDevice = "https://asia-southeast2-urse-project.cloudfunctions
 export const tableDevice = `
 <tr>
     <td>
-        <div class="flex items-center gap-3">
-            <div class="flex flex-col">
-                <h4 class="font-semibold">#NAME#</h4>
-            </div>
+        <div
+            class="inline-flex items-center h-6 px-3 text-label-md text-pink-700 dark:text-pink-200 bg-pink-100 dark:bg-opacity-20 rounded-full">
+            #NAME#
         </div>
     </td>
     <td>
