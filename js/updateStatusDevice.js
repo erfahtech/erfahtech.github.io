bf54a50a2f1c12b20fdcb8056d71c800df1f8ae1
erditona <erditonaushaadam@gmail.com>
2023-11-20 22:16:27 +0700

    Refactor updateStatusDevice function

--- js/updateStatusDevice.js
+++ js/updateStatusDevice.js
@@ -1,15 +1,12 @@
 // apiService.js
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
-export async function updateStatusDevice(idDevice, status) {
-  const token = getCookie("token");
-  const updateStatusUrl = `https://example.com/api/updateStatus?id=${idDevice}`;
-
+const updatetWithBearer = async (target_url, token, datajson, responseFunction) => {
   const myHeaders = new Headers();
   myHeaders.append("Authorization", `Bearer ${token}`);
   myHeaders.append("Content-Type", "application/json");
 
-  const raw = JSON.stringify({ status });
+  const raw = JSON.stringify(datajson);
 
   const requestOptions = {
     method: "PUT",
@@ -19,12 +16,36 @@ export async function updateStatusDevice(idDevice, status) {
   };
 
   try {
-    const response = await fetch(updateStatusUrl, requestOptions);
+    const response = await fetch(target_url, requestOptions);
     const result = await response.text();
-    console.log("Status berhasil diupdate:", JSON.parse(result));
-    return JSON.parse(result);
+    responseFunction(JSON.parse(result));
+    return response; // Return the entire response object
   } catch (error) {
-    console.error("Terjadi kesalahan:", error.message);
+    console.log("error", error);
     throw error; // Rethrow the error to be caught in the calling function
   }
-}
+};
+
+const updateStatusDevice = async (DEVICEID, STATUS) => {
+  const token = getCookie("token");
+  const target_url = `https://asia-southeast2-urse-project.cloudfunctions.net/urse-updatedevice?id=${DEVICEID}`;
+
+  const datajson = {
+    status: STATUS,
+  };
+
+  const response = await updatetWithBearer(target_url, token, datajson, (result) => {
+    responseData(result);
+  });
+  return response;
+};
+
+const responseData = (result) => {
+  if (result) {
+    console.log(result.message);
+  } else {
+    console.error(result.message);
+  }
+};
+
+export { updatetWithBearer, updateStatusDevice };
