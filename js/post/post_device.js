26cb4fc2e5e610ac4116c053bc2cc75b2983dd4d
aulianafahrian <aulianafahrian@gmail.com>
2023-11-24 07:32:45 +0700

    Refactor postDevices function and add topic validation

--- js/post/post_device.js
+++ js/post/post_device.js
@@ -1,6 +1,74 @@
+// import { postWithBearer } from "https://jscroot.github.io/api/croot.js";
+// import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const loadingElement = document.getElementById("loading");
+//   if (loadingElement) {
+//     loadingElement.style.display = "none";
+//   }
+// });
+
+// const postDevices = () => {
+//   const nama = document.getElementById("isiName").value;
+//   const topic = document.getElementById("isiTopic").value;
+//   const loadingElement = document.getElementById("loading");
+//   const diabuttonElement = document.getElementById("diabutton");
+//   const email = localStorage.getItem("userEmail");
+
+//   diabuttonElement.style.display = "none";
+//   loadingElement.style.display = "block";
+
+//   if (nama === "" || topic === "") {
+//     Swal.fire({
+//       icon: "error",
+//       title: "Gagal Menambahkan Device",
+//       text: "Please fill in all fields.",
+//     });
+
+//     loadingElement.style.display = "none";
+//     diabuttonElement.style.display = "flex";
+//     return;
+//   }
+
+//   const target_url = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-insertdevices";
+//   const datainjson = {
+//     name: nama,
+//     topic: "urse/" + email + "/" + topic,
+//   };
+
+//   postWithBearer(target_url, getCookie("token"), datainjson, (result) => {
+//     responseData(result);
+//     loadingElement.style.display = "none";
+//     diabuttonElement.style.display = "flex";
+//   });
+// };
+
+// const responseData = (result) => {
+//   if (result) {
+//     Swal.fire({
+//       icon: "success",
+//       title: "Tambah Device Berhasil",
+//       text: result.message,
+//     }).then((result) => {
+//       if (result.isConfirmed || result.isDismissed) {
+//         window.location.href = "device_control.html";
+//       }
+//     });
+//   } else {
+//     Swal.fire({
+//       icon: "error",
+//       title: "Tambah Device Gagal",
+//       text: result.message,
+//     });
+//   }
+// };
+
+// window.postDevices = postDevices;
+
+
 const postDevices = () => {
     const nama = document.getElementById("isiName").value;
-    let topic = document.getElementById("isiTopic").value.toLowerCase(); // Convert to lowercase
+    let topic = document.getElementById("isiTopic").value;
     const loadingElement = document.getElementById("loading");
     const diabuttonElement = document.getElementById("diabutton");
     const email = localStorage.getItem("userEmail");
@@ -20,13 +88,13 @@ const postDevices = () => {
         return;
     }
 
-    // Validate topic
-    const topicRegex = /^[a-z]+$/; // Only lowercase letters
+    // Validate topic using regex
+    const topicRegex = /^[a-z]+$/;
     if (!topicRegex.test(topic)) {
         Swal.fire({
             icon: "error",
-            title: "Invalid Topic",
-            text: "Topic harus menggunakan huruf kecil dan tidak menggunakan angka.",
+            title: "Gagal Menambahkan Device",
+            text: "Topic should only contain lowercase letters and no numbers.",
         });
 
         loadingElement.style.display = "none";
@@ -34,10 +102,12 @@ const postDevices = () => {
         return;
     }
 
+    // If validation passes, proceed with the API call
+    topic = "urse/" + email + "/" + topic;
     const target_url = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-insertdevices";
     const datainjson = {
         name: nama,
-        topic: "urse/" + email + "/" + topic,
+        topic: topic,
     };
 
     postWithBearer(target_url, getCookie("token"), datainjson, (result) => {
@@ -46,6 +116,7 @@ const postDevices = () => {
         diabuttonElement.style.display = "flex";
     });
 };
+
 const responseData = (result) => {
     if (result) {
         Swal.fire({
@@ -65,5 +136,12 @@ const responseData = (result) => {
         });
     }
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loadingElement = document.getElementById("loading");
+    if (loadingElement) {
+        loadingElement.style.display = "none";
+    }
+});
 
 window.postDevices = postDevices;
