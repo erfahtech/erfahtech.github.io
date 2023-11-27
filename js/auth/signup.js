8055bb0fff7f090cbd5962a0be633ae12235a72e
aulianafahrian <aulianafahrian@gmail.com>
2023-11-27 16:36:12 +0700

    Refactor form validation and signup functionality

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -1,12 +1,190 @@
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 
+// const validateInput = (inputId, validationFunction, validationMessageId, errorMessage) => {
+//   const inputElement = document.getElementById(inputId);
+//   const validationMessageElement = document.getElementById(validationMessageId);
+
+//   const validateAndDisplayMessage = () => {
+//     const inputValue = inputElement.value;
+
+//     // Check if the input is empty
+//     if (inputValue === '') {
+//       validationMessageElement.innerText = '';
+//       return;
+//     }
+
+//     const isValid = validationFunction(inputValue);
+
+//     if (!isValid) {
+//       validationMessageElement.innerText = errorMessage;
+//     } else {
+//       validationMessageElement.innerText = '';
+//     }
+//   };
+
+//   inputElement.addEventListener('input', validateAndDisplayMessage);
+
+//   // Handling input value deletion
+//   inputElement.addEventListener('change', validateAndDisplayMessage);
+// };
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   validateInput('emailsignup', validateEmail, 'emailValidationMessage', 'Email: Harus mengandung "@" dan "."');
+//   validateInput('phonesignup', validatePhone, 'phoneValidationMessage', 'Nomor telepon diawali 62, 7-13 karakter');
+//   validateInput('usernamesignup', validateUsername, 'usernameValidationMessage', 'Username: dua kata, awalan huruf besar');
+//   validateInput('passwordsignup', validatePassword, 'passwordValidationMessage', 'Password: 8+ karakter, huruf & angka');
+// });
+
+// const postSignUp = () => {
+//   const email = getValue("emailsignup");
+//   const phonenumber = getValue("phonesignup");
+//   const username = getValue("usernamesignup");
+//   const password = getValue("passwordsignup");
+//   const loadingElement = document.getElementById("loading");
+
+//   loadingElement.style.display = "block";
+
+//   if (!email || !phonenumber || !username || !password) {
+//     Swal.fire({
+//       icon: "error",
+//       title: "Signup Failed",
+//       text: "Please fill in all fields.",
+//     });
+
+//     loadingElement.style.display = "none";
+//     return;
+//   }
+
+//   const target_url = "https://asia-southeast2-urse-project.cloudfunctions.net/urse-signup";
+//   const datainjson = {
+//     email,
+//     phonenumber,
+//     username,
+//     password,
+//   };
+
+//   fetch(target_url, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(datainjson),
+//   })
+//     .then((response) => response.json())
+//     .then((result) => {
+//       responseData(result);
+//     })
+//     .finally(() => {
+//       loadingElement.style.display = "none";
+//     });
+// };
+
+// const responseData = (result) => {
+//   const loadingElement = document.getElementById("loading");
+
+//   if (result.status === true) {
+//     Swal.fire({
+//       icon: "success",
+//       title: "SignUp Successful",
+//       text: "You have successfully signed up.",
+//     }).then((result) => {
+//       if (result.isConfirmed || result.isDismissed) {
+//         window.location.href = "login.html";
+//       }
+//     });
+//   } else {
+//     Swal.fire({
+//       icon: "error",
+//       title: "Signup Failed",
+//       text: result.message,
+//     });
+//     loadingElement.style.display = "none";
+//   }
+// };
+
+// window.postSignUp = postSignUp;
+
+// const passwordFunc = () => {
+//   const x = document.getElementById("passwordsignup");
+//   const parent = x.parentNode;
+
+//   x.type = x.type === "password" ? "text" : "password";
+//   parent.classList.toggle("show", x.type === "text");
+// };
+
+// window.passwordFunc = passwordFunc;
+
+// const postSignUpWithValidation = () => {
+//   if (validateForm()) {
+//     postSignUp();
+//   }
+// }
+
+// window.postSignUpWithValidation = postSignUpWithValidation;
+
+// const validateEmail = (email) => {
+//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   return emailRegex.test(email);
+// }
+
+// const validatePhone = (phone) => {
+//   const phoneRegex = /^62\d{5,11}$/;
+//   return phoneRegex.test(phone);
+// }
+
+// const validateUsername = (username) => {
+//   const usernameRegex = /^[A-Z][a-z]*\s[A-Z][a-z]*$/;
+//   return usernameRegex.test(username);
+// }
+
+// const validatePassword = (password) => {
+//   const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
+//   return passwordRegex.test(password);
+// }
+
+
+// Function to validate the entire form
+const validateForm = () => {
+  const emailIsValid = validateEmail(getValue("emailsignup"));
+  const phoneIsValid = validatePhone(getValue("phonesignup"));
+  const usernameIsValid = validateUsername(getValue("usernamesignup"));
+  const passwordIsValid = validatePassword(getValue("passwordsignup"));
+
+  // Add more validation checks as needed
+
+  // Return true if all validations pass, otherwise return false
+  return emailIsValid && phoneIsValid && usernameIsValid && passwordIsValid;
+};
+
+// Validation functions
+const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+const validatePhone = (phone) => {
+  const phoneRegex = /^62\d{5,11}$/;
+  return phoneRegex.test(phone);
+};
+
+const validateUsername = (username) => {
+  const usernameRegex = /^[A-Z][a-z]*\s[A-Z][a-z]*$/;
+  return usernameRegex.test(username);
+};
+
+const validatePassword = (password) => {
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
+  return passwordRegex.test(password);
+};
+
+// Function to validate input and display validation messages
 const validateInput = (inputId, validationFunction, validationMessageId, errorMessage) => {
   const inputElement = document.getElementById(inputId);
   const validationMessageElement = document.getElementById(validationMessageId);
 
   const validateAndDisplayMessage = () => {
     const inputValue = inputElement.value;
-    
+
     // Check if the input is empty
     if (inputValue === '') {
       validationMessageElement.innerText = '';
@@ -28,13 +206,16 @@ const validateInput = (inputId, validationFunction, validationMessageId, errorMe
   inputElement.addEventListener('change', validateAndDisplayMessage);
 };
 
+// Document ready event
 document.addEventListener('DOMContentLoaded', () => {
+  // Validation for each input field
   validateInput('emailsignup', validateEmail, 'emailValidationMessage', 'Email: Harus mengandung "@" dan "."');
   validateInput('phonesignup', validatePhone, 'phoneValidationMessage', 'Nomor telepon diawali 62, 7-13 karakter');
   validateInput('usernamesignup', validateUsername, 'usernameValidationMessage', 'Username: dua kata, awalan huruf besar');
   validateInput('passwordsignup', validatePassword, 'passwordValidationMessage', 'Password: 8+ karakter, huruf & angka');
 });
 
+// Function to handle sign up
 const postSignUp = () => {
   const email = getValue("emailsignup");
   const phonenumber = getValue("phonesignup");
@@ -79,6 +260,7 @@ const postSignUp = () => {
     });
 };
 
+// Function to handle response after sign up
 const responseData = (result) => {
   const loadingElement = document.getElementById("loading");
 
@@ -102,8 +284,7 @@ const responseData = (result) => {
   }
 };
 
-window.postSignUp = postSignUp;
-
+// Function to toggle password visibility
 const passwordFunc = () => {
   const x = document.getElementById("passwordsignup");
   const parent = x.parentNode;
@@ -112,32 +293,16 @@ const passwordFunc = () => {
   parent.classList.toggle("show", x.type === "text");
 };
 
+// Expose functions to the global scope
+window.postSignUp = postSignUp;
 window.passwordFunc = passwordFunc;
 
+// Function to handle sign up with validation
 const postSignUpWithValidation = () => {
   if (validateForm()) {
     postSignUp();
   }
-}
+};
 
+// Expose function to the global scope
 window.postSignUpWithValidation = postSignUpWithValidation;
-
-const validateEmail = (email) => {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
-}
-
-const validatePhone = (phone) => {
-  const phoneRegex = /^62\d{5,11}$/;
-  return phoneRegex.test(phone);
-}
-
-const validateUsername = (username) => {
-  const usernameRegex = /^[A-Z][a-z]*\s[A-Z][a-z]*$/;
-  return usernameRegex.test(username);
-}
-
-const validatePassword = (password) => {
-  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
-  return passwordRegex.test(password);
-}
