e80fedbc1239d8014b91b010b81f43aa757d1127
aulianafahrian <aulianafahrian@gmail.com>
2023-11-27 22:25:36 +0700

    Refactor editDevice function and import getCookie from external source

--- js/edit/editDevice2.js
+++ js/edit/editDevice2.js
@@ -1,3 +1,126 @@
+// import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+// const updateWithBearer = async (target_url, token, datajson, responseFunction) => {
+//     const myHeaders = new Headers();
+//     myHeaders.append("Authorization", `Bearer ${token}`);
+//     myHeaders.append("Content-Type", "application/json");
+
+//     const raw = JSON.stringify(datajson);
+
+//     const requestOptions = {
+//         method: "PUT",
+//         headers: myHeaders,
+//         body: raw,
+//         redirect: "follow",
+//     };
+
+//     try {
+//         const response = await fetch(target_url, requestOptions);
+//         const result = await response.text();
+//         responseFunction(JSON.parse(result));
+//         return response; // Return the entire response object
+//     } catch (error) {
+//         console.log("error", error);
+//         throw error; // Rethrow the error to be caught in the calling function
+//     }
+// };
+
+// const editDevice = async (IDEDIT, NAME, TOPIC) => {
+//     const deviceId = IDEDIT;
+//     const deviceName = NAME;
+//     const deviceTopic = TOPIC;
+//     console.log("Edit device called:", IDEDIT, NAME, TOPIC);
+
+//     const data = {
+//         name: NAME,
+//         topic: TOPIC,
+//     };
+//     const { value: combinedInput, isConfirmed: isInputConfirmed } = await Swal.fire({
+//         title: "Edit Device",
+//         html: `<div>
+//                 <input id="swal-input1" class="swal2-input" placeholder="New Name" value="${deviceName}">
+//                 <input id="swal-input2" class="swal2-input" placeholder="New Topic" value="${deviceTopic}">
+//                 </div>`,
+//         showCancelButton: true,
+//         confirmButtonColor: "#6750A4",
+//         cancelButtonColor: "#999999",
+//         confirmButtonText: "Simpan",
+//         cancelButtonText: "Batal",
+//         preConfirm: (input) => {
+//             // Update data object with new values
+//             data.name = newName;
+//             data.topic = newTopic;
+
+//             const newName = input[0];
+//             const newTopic = input[1];
+
+//             if (!newName || !newTopic) {
+//                 Swal.showValidationMessage("Nama dan Topic perangkat tidak boleh kosong!");
+//                 return false;
+//             }
+
+//             // Validate that the Topic allows only lowercase letters, numbers, and symbols
+//             const topicRegex = /^[a-z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/`-]+$/;
+//             if (!topicRegex.test(newTopic)) {
+//                 Swal.showValidationMessage("Topic perangkat hanya boleh mengandung huruf kecil, angka, dan simbol!");
+//                 return false;
+//             }
+
+//             return true;
+//         },
+//         didOpen: () => {
+//             const inputs = Swal.getPopup().querySelectorAll("input");
+//             inputs[0].focus();
+//         },
+//     });
+
+//     if (isInputConfirmed) {
+//         const [newName, newTopic] = combinedInput;
+//         const isConfirmed = await Swal.fire({
+//             title: `Konfirmasi`,
+//             html: `<div>Apakah Anda yakin mengubah?</div>
+//                     <div>Nama: ${newName}</div>
+//                     <div>Topic: ${newTopic}</div>`,
+//             icon: "warning",
+//             showCancelButton: true,
+//             confirmButtonColor: "#6750A4",
+//             cancelButtonColor: "#999999",
+//             confirmButtonText: "Ya, Edit!",
+//             cancelButtonText: "Batal",
+//         });
+
+//         if (isInputConfirmed) { // Change isConfirmed.isConfirmed to isInputConfirmed
+//             const token = getCookie("token");
+//             const target_url = `https://asia-southeast2-urse-project.cloudfunctions.net/urse-updatedevice?id=${deviceId}`;
+//             const requestBody = {
+//                 name: newName,
+//                 topic: newTopic,
+//             };
+
+//             try {
+//                 const response = await updateWithBearer(target_url, token, requestBody, (result) => result);
+
+//                 if (response && response.status) {
+//                     await Swal.fire({
+//                         icon: "success",
+//                         title: response.message || "Perangkat berhasil diubah",
+//                         showConfirmButton: false,
+//                         timer: 1500,
+//                     });
+//                     location.reload();
+//                 } else {
+//                     console.error("Request failed with status:", response && response.status);
+//                     throw new Error("Request failed with status: " + (response && response.status));
+//                 }
+//             } catch (error) {
+//                 console.error("Error:", error);
+//             }
+//         }
+//     }
+// };
+
+// window.editDevice = editDevice;
+
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 const updateWithBearer = async (target_url, token, datajson, responseFunction) => {
@@ -20,7 +143,7 @@ const updateWithBearer = async (target_url, token, datajson, responseFunction) =
         responseFunction(JSON.parse(result));
         return response; // Return the entire response object
     } catch (error) {
-        console.log("error", error);
+        console.error("Error:", error);
         throw error; // Rethrow the error to be caught in the calling function
     }
 };
@@ -32,27 +155,23 @@ const editDevice = async (IDEDIT, NAME, TOPIC) => {
     console.log("Edit device called:", IDEDIT, NAME, TOPIC);
 
     const data = {
-        name: NAME,
-        topic: TOPIC,
+        name: deviceName,
+        topic: deviceTopic,
     };
+
     const { value: combinedInput, isConfirmed: isInputConfirmed } = await Swal.fire({
         title: "Edit Device",
         html: `<div>
                 <input id="swal-input1" class="swal2-input" placeholder="New Name" value="${deviceName}">
                 <input id="swal-input2" class="swal2-input" placeholder="New Topic" value="${deviceTopic}">
-                </div>`,
+            </div>`,
         showCancelButton: true,
         confirmButtonColor: "#6750A4",
         cancelButtonColor: "#999999",
         confirmButtonText: "Simpan",
         cancelButtonText: "Batal",
         preConfirm: (input) => {
-            // Update data object with new values
-            data.name = newName;
-            data.topic = newTopic;
-
-            const newName = input[0];
-            const newTopic = input[1];
+            const [newName, newTopic] = input;
 
             if (!newName || !newTopic) {
                 Swal.showValidationMessage("Nama dan Topic perangkat tidak boleh kosong!");
@@ -65,6 +184,10 @@ const editDevice = async (IDEDIT, NAME, TOPIC) => {
                 Swal.showValidationMessage("Topic perangkat hanya boleh mengandung huruf kecil, angka, dan simbol!");
                 return false;
             }
+
+            // Update data object with new values
+            data.name = newName;
+            data.topic = newTopic;
 
             return true;
         },
@@ -89,7 +212,7 @@ const editDevice = async (IDEDIT, NAME, TOPIC) => {
             cancelButtonText: "Batal",
         });
 
-        if (isInputConfirmed) { // Change isConfirmed.isConfirmed to isInputConfirmed
+        if (isConfirmed.value) { // Change isConfirmed.isConfirmed to isConfirmed.value
             const token = getCookie("token");
             const target_url = `https://asia-southeast2-urse-project.cloudfunctions.net/urse-updatedevice?id=${deviceId}`;
             const requestBody = {
@@ -120,3 +243,4 @@ const editDevice = async (IDEDIT, NAME, TOPIC) => {
 };
 
 window.editDevice = editDevice;
+
