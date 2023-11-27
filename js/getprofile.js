a5de9641f8935e428b77c23ae9d5072f61d97eaf
erditona <erditonaushaadam@gmail.com>
2023-11-27 20:06:42 +0700

    Add getprofile.js and get-profile.js files

---
+++ js/getprofile.js
@@ -0,0 +1,17 @@
+// getprofile.js
+
+export const URLGetProfile = "https://api.example.com/get-profile";
+
+export const responseProfileData = (data) => {
+  // Pastikan data adalah array dan memiliki elemen pertama
+  if (Array.isArray(data) && data.length > 0) {
+    const userData = data[0]; // Ambil elemen pertama dari array
+
+    // Menggunakan getElementById untuk mengatur nilai dalam elemen HTML
+    document.getElementById("username").textContent = userData.username;
+    document.getElementById("email").textContent = userData.email;
+    document.getElementById("phonenumber").textContent = userData.phonenumber;
+  } else {
+    console.error("Data pengguna tidak valid atau tidak ditemukan.");
+  }
+};
