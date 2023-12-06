30ffa684e5a1d2a265a622e9b36942f0a39a94dc
erditona <erditonaushaadam@gmail.com>
2023-12-06 22:52:58 +0700

    Delete folder js

--- js/edit/urleditDevice.js
+++
@@ -1,5 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const user = urlParams.get("user");
-
-export const urlFetch =
-  "https://asia-southeast2-urse-project.cloudfunctions.net/urse-getdevices?id=" + deviceId;
